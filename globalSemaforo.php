1ad8b5b20bf24516749aab8a44d8b3e96918f25d
GussHdez2 <94919885+GussHdez2@users.noreply.github.com>
2021-12-08 13:08:51 -0600

    Delete globalSemaforo.php

--- globalSemaforo.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-
-    $semaforo = array('semaforo' => $_SESSION['semaforo_user'],
-                      'tipoUser' => $_SESSION['tipo_user']);
-    echo json_encode($semaforo);
-?>
